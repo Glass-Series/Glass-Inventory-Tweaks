30f5fc6deeb908212e482240f69814ae7d4b3ce1
calmilamsy <bumbill00@gmail.com>
2026-05-31 23:54:26 +0100

    Make mousewheel scrolling mostly work

---
+++ src/main/java/net/glasslauncher/mods/glassinventorytweaks/mixin/ScreenMixin.java
@@ -0,0 +1,24 @@
+package net.glasslauncher.mods.glassinventorytweaks.mixin;
+
+import net.glasslauncher.mods.glassinventorytweaks.impl.ScrollImpl;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import org.lwjgl.input.Mouse;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Screen.class)
+public class ScreenMixin {
+
+    @Inject(method = "onMouseEvent", at = @At(value = "HEAD", target = "Lnet/minecraft/client/gui/screen/Screen;onMouseEvent()V"), cancellable = true)
+    private void test(CallbackInfo ci) {
+        if ((Object) this instanceof HandledScreen handledScreen) {
+            float wheel = Mouse.getDWheel();
+            if (wheel != 0 && ScrollImpl.handleScroll(handledScreen, wheel)) {
+                ci.cancel();
+            }
+        }
+    }
+}
